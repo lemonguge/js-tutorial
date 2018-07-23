d823027c968bd7536d455b90b9d294e65d3327b7
倜傥 <jiehong.jh@alibaba-inc.com>
2018-07-24 06:58:29 +0800

    函数章节完成

---
+++ js/filter.js
@@ -0,0 +1,20 @@
+console.log('' && 1); // ''
+
+var arr = ['A', '', 'B', null, undefined, 'C', '  '];
+var r = arr.filter(function (s) {
+    return s && s.trim(); // 注意：IE9以下的版本没有trim()方法
+});
+console.log(r);
+
+console.log('-----------------')
+
+arr = ['A', 'B', 'C'];
+r = arr.filter(function (element, index, self) {
+    console.log(element); // 依次打印'A', 'B', 'C'
+    console.log(index); // 依次打印0, 1, 2
+    console.log(self); // self就是变量arr
+    return true;
+});
+
+console.log('-----------------')
+console.log(r);
